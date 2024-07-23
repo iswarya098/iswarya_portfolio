98bb9217a833a0b714c97b52005be7841db65c70
iswarya098 <iswaryachelluboina098@gmail.com>
2024-07-23 18:00:09 +0530

    All files added and commited

---
+++ script2.js
@@ -0,0 +1,23 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuBars = document.getElementById("menu-bars");
+    const navbar = document.querySelector(".navbar");
+
+    menuBars.addEventListener("click", () => {
+        menuBars.classList.toggle("fa-times");
+        navbar.classList.toggle("active");
+    });
+
+    window.onscroll = () => {
+        menuBars.classList.remove("fa-times");
+        navbar.classList.remove("active");
+    };
+
+    document.querySelectorAll(".navbar a").forEach(link => {
+        link.addEventListener("click", () => {
+            menuBars.classList.remove("fa-times");
+            navbar.classList.remove("active");
+        });
+    });
+});
